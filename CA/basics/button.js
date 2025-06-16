3ddc66dde10d0ecbd9cd472411e59163eac30016
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-16 03:10:23 +0300

    Create button.js

---
+++ CA/basics/button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SubmitButton() {
+  function handleClick() {
+    alert('Submission Successful.');
+  }
+  return <button onClick={handleClick}>Submit</button>;
+}
+
+export default SubmitButton;
+
+
+
+
+

37e7db108430d0740e7d09da57b1dc80e3fb9327
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-16 07:44:59 +0300

    Create importExport.js

---
+++ CA/basics/importExport.js
@@ -0,0 +1,26 @@
+// In EmployeePage.js
+import React from 'react';
+
+function EmployeePage() {
+  // Code for the component
+}
+
+export default EmployeePage;
+
+// In PromoPage.js
+import React from 'react';
+
+function PromoPage() {
+  // Code for the component
+}
+
+export default PromoPage;
+
+// In App.js
+import React from 'react';
+import EmployeePage from './EmployeePage';
+import PromoPage from './PromoPage';
+
+function App() {
+  // Code for the component
+}
